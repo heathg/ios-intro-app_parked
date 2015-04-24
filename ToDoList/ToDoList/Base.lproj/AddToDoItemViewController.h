f085154d809aea52471b5e22f3ccb29220b78f59
heathg <Heathglover@Heaths-MacBook-Pro.local>
2015-04-24 18:56:42 +1000

    Completed "Structuring an App" Section

--- ToDoList/ToDoList/Base.lproj/AddToDoItemViewController.h
+++ ToDoList/ToDoList/Base.lproj/AddToDoItemViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AddToDoItemViewController.h
 //  ToDoList
 //
 //  Created by Heath Glover on 24/04/2015.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AddToDoItemViewController : UIViewController
 
 @end
-
